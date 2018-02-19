0647bf4ed1f7f5a4936c40626ae25e329377aced
Christine Dodrill <me@christine.website>
2018-02-18 19:06:24 -0800

    internal/database/migrations: add is_admin

--- internal/database/migrations/1519009044_users.up.sql
+++ internal/database/migrations/1519009044_users.up.sql
@@ -1,15 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users
- ( id            SERIAL
- , uuid          UUID      DEFAULT uuid_generate_v1mc()
- , created_at    TIMESTAMP NOT NULL DEFAULT NOW()
- , edited_at     TIMESTAMP NOT NULL DEFAULT NOW()
- , username      TEXT      NOT NULL
- , discord_id    TEXT      NOT NULL
- , banned        BOOLEAN   NOT NULL DEFAULT FALSE
- , banned_mod_id UUID
- , banned_reason TEXT
+ ( id         SERIAL
+ , uuid       UUID      DEFAULT uuid_generate_v1mc()
+ , created_at TIMESTAMP NOT NULL DEFAULT NOW()
+ , edited_at  TIMESTAMP NOT NULL DEFAULT NOW()
+ , username   TEXT      NOT NULL
+ , discord_id TEXT      NOT NULL
+ , is_admin   BOOLEAN   NOT NULL DEFAULT FALSE
  );
 
 CREATE INDEX IF NOT EXISTS users_uuid ON users(uuid);
