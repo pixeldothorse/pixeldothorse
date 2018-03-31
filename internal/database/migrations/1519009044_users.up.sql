9d9dc0c7941febfeedaf11d330a55c1df3876b57
Christine Dodrill <me@christine.website>
2018-03-31 14:50:37 -0700

    internal/database/migrations: add generic updated_at trigger for tables

--- internal/database/migrations/1519009044_users.up.sql
+++ internal/database/migrations/1519009044_users.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users
  ( id         SERIAL
  , uuid       UUID      DEFAULT uuid_generate_v1mc()
  , created_at TIMESTAMP NOT NULL DEFAULT NOW()
- , edited_at  TIMESTAMP NOT NULL DEFAULT NOW()
+ , updated_at TIMESTAMP NOT NULL DEFAULT NOW()
  , username   TEXT      NOT NULL
  , discord_id TEXT      NOT NULL
  , is_admin   BOOLEAN   NOT NULL DEFAULT FALSE
